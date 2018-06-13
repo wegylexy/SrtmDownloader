aaf3080bec1bd78cba392077ab280b4c04ad9b60
TimTim Wong <t2@live.hk>
2018-06-13 11:27:46 +0800

    Add project files.

---
+++ Srtm3Downloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Srtm3Downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
